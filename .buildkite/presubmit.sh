8e365b7bbd65cba091058b2e5f0b5c8aa7359dcf
Jakob Buchgraber <bucjac@gmail.com>
2018-02-07 21:08:37 +0100

    Create presubmit.sh

---
+++ .buildkite/presubmit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -xuo pipefail
+
+echo '--- Cleanup'
+bazel clean --expunge
+rm -rf bep.json
+
+echo '+++ Building'
+bazel build --color=yes //src:bazel || exit $?
+
+echo '+++ Testing'
+bazel test --color=yes --build_event_json_file=bep.json //scripts/... //src/test/... //third_party/ijar/... //tools/android/...
+      
+TESTS_EXIT_STATUS=$?
+      
+python3 .buildkite/failed_testlogs.py bep.json | while read logfile; do buildkite-agent artifact upload $logfile; done
+      
+exit $TESTS_EXIT_STATUS
