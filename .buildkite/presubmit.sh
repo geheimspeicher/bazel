28c43caaa51102a5b961fa51b851765c21793257
Jakob Buchgraber <buchgr@google.com>
2018-02-07 22:43:31 +0100

    asdf

--- .buildkite/presubmit.sh
+++ .buildkite/presubmit.sh
@@ -10,11 +10,11 @@ bazel build --color=yes //src:bazel || exit $?
 
 echo '+++ Testing'
 bazel test --color=yes --build_event_json_file=bep.json //scripts/... //src/test/... //third_party/ijar/... //tools/android/...
-      
+
 TESTS_EXIT_STATUS=$?
 
 echo '--- Uploading Failed Test Logs'
 
 python3 .buildkite/failed_testlogs.py bep.json | while read logfile; do buildkite-agent artifact upload $logfile; done
-      
+
 exit $TESTS_EXIT_STATUS
